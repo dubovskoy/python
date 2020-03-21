5fa254f45ef8af3623192abf503e93dc51e566d3
sdubovskoy <tptpee@gmail.com>
2020-03-21 19:21:53 +0300

    lesson-2

--- task_3.py
+++ task_3.py
@@ -1,12 +1,13 @@
-# 3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
-# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+# 3. Пользователь вводит месяц в виде целого числа от 1 до 12. Сообщить к какому времени года
+# относится месяц (зима, весна, лето, осень). Напишите решения через list и через dict.
 
-number = input('Введите число: ')
+season_list = ['', 'Зима', 'Зима', 'Весна', 'Весна', 'Весна', 'Лето', 'Лето', 'Лето', 'Осень', 'Осень', 'Осень', 'Зима']
+season_dict = {1: 'Зима', 2: 'Зима', 3: 'Весна', 4: 'Весна', 5: 'Весна', 6: 'Лето', 7: 'Лето', 8: 'Лето', 9: 'Осень',
+               10: 'Осень', 11: 'Осень', 12: 'Зима'}
 
-numberX = int(number)
-numberXX = int(number + number)
-numberXXX = int(number + number + number)
-
-number_sum = numberX + numberXX + numberXXX
-
-print(f'Сумма чисел ({numberX} + {numberXX} + {numberXXX}) =', number_sum)
+month_number = int(input('Введите номер месяца: '))
+if 1 <= month_number <= 12:
+    print(f'Список. Месяц -={month_number}=- относится к времени года: {season_dict[month_number]}')
+    print(f'Словарь. Месяц -={month_number}=- относится к времени года: {season_list[month_number]}')
+else:
+    print(f'Месяца с номером -={month_number}=- не существует.')
